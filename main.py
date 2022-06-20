d40aafcf1866ee6844cecda535195d69fcdafa2f
elijah24 <107879320+elijah2408@users.noreply.github.com>
2022-06-20 20:10:42 +0000

    update

--- main.py
+++ main.py
@@ -13,7 +13,21 @@ print(str(userAGE) + " is so young!")
 # Create your own MadLibs
 # Have at least 5 inputs 
 
-noun = input("Please give me a noun: ")
+noun1 = input("Who is the lead in Rush hour? ")
+
+noun2 = input("What was his job?")
+
+noun3 = input("Who is his new bbodyguard?")
+
+noun4 = input("Where is the location at?")
+
+noun5 = input("Why was there a huge altercation between the two?")
+              
+noun6 = input("why were they fighting at a mcdonalds")
+
+noun7 = input("")
 
 
 
+
+noun
