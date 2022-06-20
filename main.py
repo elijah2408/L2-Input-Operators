904875be909f2c7875b557b5df2e96a62e8a2509
elijah24 <107879320+elijah2408@users.noreply.github.com>
2022-06-20 19:03:37 +0000

    madlib

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+userNAME = input("Enter your name: ")
+# print("Hey " + userNAME)
+
+userGRADE = input("What grade are you in?")
+print("Hey " + userNAME + ", " + userGRADE + " is a lame grade")
+
+userYEAR = int(input("What year were you born? "))
+userAGE = 2022 - userYEAR 
+print(str(userAGE) + " is so young!")
+'''
+
+# Create your own MadLibs
+# Have at least 5 inputs 
+
+noun = input("Please give me a noun: ")
+
+
+
